4de0dddd789f2a6a7cb67b879aa5eb6984c5a0e0
UiUian <60983308+Vivian-zzz@users.noreply.github.com>
2021-10-17 08:30:19 +0800

    edit EC partc

---
+++ Collisions/Collisions2/Assets/Scripts/MinkowskiTool.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MinkowskiTool
+{
+    // 生成闵可夫斯基差集
+    public static List<Vector2> computeMinkowskiSet(Prism prismA, Prism prismB)
+    {
+        List<Vector2> vertices = new List<Vector2>();
+        foreach (var v1 in prismA.GetVec2Points())
+        {
+            foreach (var v2 in prismB.GetVec2Points())
+            {
+                vertices.Add(v1 - v2);
+            }
+        }
+        return ConvexHullGenerator.GenConvexHull(vertices.ToArray());
+    }
+
+}
---
+++ Collisions/Collisions2/Assets/Scripts/Prisms/Prism.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Prism : MonoBehaviour
+{
+    public int pointCount = 3;
+    public Vector3[] points;
+
+    public float midY, height;
+
+    public GameObject prismObject;
+    public Vector2 getFarthestPointInDirection(Vector2 dir)
+    {
+        float maxDistance = float.MinValue;
+        int maxIndex = 0;
+        for (int i = 0; i < GetVec2Points().Count; ++i)
+        {
+            float distance = Vector2.Dot(GetVec2Points()[i], dir);
+            if (distance > maxDistance)
+            {
+                maxDistance = distance;
+                maxIndex = i;
+            }
+        }
+        return GetVec2Points()[maxIndex];
+    }
+
+    public List<Vector2> GetVec2Points()
+    {
+        List<Vector2> vec2Points = new List<Vector2>();
+
+        foreach(var point in points)
+        {
+            vec2Points.Add(new Vector2(point.x, point.z));
+        }
+
+        return vec2Points;
+    }
+
+
+}
